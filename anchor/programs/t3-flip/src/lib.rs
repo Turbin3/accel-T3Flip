febb3e97148f73dd9e3944ffb88df3d6f0404477
Logan Yu <yu.logan@gmail.com>
2025-10-30 11:07:55 -0400

    undo change address

--- anchor/programs/t3-flip/src/lib.rs
+++ anchor/programs/t3-flip/src/lib.rs
@@ -9,7 +9,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("ALBrSaQ89ntEGMDxj1idPdPVeHkFtfkjEkLpns1EvJTu");
+declare_id!("69bDTx8B9AEGkUDWiHufnzvx69HYhGtSsgJ7KuDrZiWL");
 
 #[program]
 pub mod t3_flip {
