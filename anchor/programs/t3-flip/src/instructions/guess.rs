23b9d10c269764ece33616628133fa33a392a773
Sekiranda <sekirandahamza@gmail.com>
2025-11-03 13:58:52 +0300

    Guess instruction logic and derive init space to use maxlen for game cards
    Signed-off-by: Sekiranda <sekirandahamza@gmail.com>

--- anchor/programs/t3-flip/src/instructions/guess.rs
+++ anchor/programs/t3-flip/src/instructions/guess.rs
@@ -8,6 +8,7 @@ pub struct Guess<'info> {
     pub player: Signer<'info>,
 
     #[account(
+        mut,
         seeds = [b"game_state", game_state.current_game_id.to_le_bytes().as_ref(), player.key().as_ref()],
         bump = game_state.bump
     )]
@@ -19,7 +20,17 @@ pub struct Guess<'info> {
 impl Guess<'_> {
     pub fn guess(&mut self, nft_id: u8) -> Result<()> {
         // match the nftid to any of the cards from the game_state
+        let mut found = false;
+        for card in self.game_state.cards.iter_mut() {
+            if *card == nft_id {
+                found = true;
+            }
+        }
         // if matched add that nft id to rewards nft array
+        if found && self.game_state.nfts_rewards.len() < 5 {
+            self.game_state.nfts_rewards.push(nft_id);
+        }
+
         // if it's wrong remove that nft id from the game state cards array
 
         Ok(())
